1e901228a041677b7c966acece9fbcd30a7c7cb9
Sakari Woods <sakariwoods@gmail.com>
2020-02-25 21:41:10 -0800

    Fixed logic for main html serve, socket.io connection success, hamburger menu started, card placement started.

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+var http = require('http');
+var express = require('express'),
+    app = module.exports.app = express();
+
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);  //pass a http.Server instance
+
 var http = require('http'),
     fs = require('fs'),
     // NEVER use a Sync function except at start-up!
     index = fs.readFileSync(__dirname + '/pages/landing.html');
+    cssfile = fs.readFileSync(__dirname + '/pages/css/landing.css');
 
 // Send index.html to all requests
 var app = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(cssfile);
     res.end(index);
 });
 
@@ -16,6 +25,10 @@ var io = require('socket.io').listen(app);
 io.on('connection', function(socket) {
   console.log("User connected.");
   socket.emit("msg", "Hello client!");
+
+  socket.on('disconnect', function(){
+      console.log("User disconnected.");
+  });
 
   socket.on('msg', function(data){
     console.log(data);
